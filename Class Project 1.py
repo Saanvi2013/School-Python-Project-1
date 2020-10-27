fd2a895dda9f6133bcbc87eb719505a3ae6cf33e
Saanvi2013 <73020457+Saanvi2013@users.noreply.github.com>
2020-10-27 19:23:19 +0530

    Add files via upload

---
+++ Class Project 1.py
@@ -0,0 +1,6 @@
+a=int(input("Enter 1st Number"))
+b=int(input("Enter 2nd Number"))
+print("the sum of 2 numbers is equal to",a+b)
+print("the difference of 2 numbers is equal to",a-b)
+print("the product of these numbers is equal to",a*b)
+print("the quotient of these numbers is equal to",a/b)
